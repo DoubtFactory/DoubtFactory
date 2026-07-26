eaccf37a271c16afccca0fc200bb60d3df830ec1
DoubtFactory <heisenbergjee@gmail.com>
2026-07-26 13:42:25 +0530

    update

---
+++ js/theme.js
@@ -0,0 +1,29 @@
+const toggle = document.getElementById("themeToggle");
+
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+
+    if(toggle){
+        toggle.textContent="☀️";
+    }
+}
+
+toggle?.addEventListener("click",()=>{
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+
+        localStorage.setItem("theme","dark");
+
+        toggle.textContent="☀️";
+
+    }else{
+
+        localStorage.setItem("theme","light");
+
+        toggle.textContent="🌙";
+
+    }
+
+});
